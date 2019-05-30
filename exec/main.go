8ca7534df402ba618dcc511d7c61aae5e19d6533
anymost <creatuture@gmail.com>
2019-05-30 12:41:54 +0800

    add more

--- exec/main.go
+++ exec/main.go
@@ -25,18 +25,30 @@ func cmd()  {
 }
 
 func contextCmd() {
-	ctx, _ := context.WithTimeout(context.Background(), time.Nanosecond)
-	cmd := exec.CommandContext(ctx,"cat", "/Users/bradyzhang/Demo/goapp/micro/main.go")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	//go func() {
-	//	cancel()
-	//}()
-	_ = cmd.Run()
+	ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Nanosecond)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "sleep", "5")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func start() {
+	cmd := exec.Command("sleep", "10")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main()  {
 	// lookPath()
 	// cmd()
-	contextCmd()
+	// contextCmd()
+	start()
 }
